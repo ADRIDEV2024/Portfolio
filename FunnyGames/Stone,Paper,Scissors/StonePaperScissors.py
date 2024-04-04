6d7f93fb86c068b63816a4fd9af5a1e641faca4a
AdriDev <adriangomis34@gmail.com>
2024-04-04 19:27:07 +0200

    Fixing Problem

--- FunnyGames/Stone,Paper,Scissors/StonePaperScissors.py
+++ FunnyGames/Stone,Paper,Scissors/StonePaperScissors.py
@@ -1,8 +1,7 @@
 import random
 
 def play():
- player1 =input(
-     "Choose an option: stone, paper or scissors.\n").lower()
+ player1 =input("Choose an option: stone, paper or scissors.\n").lower()
  
  computer= print(random.choice(["stone","paper","scissors"]))
  
@@ -13,3 +12,21 @@ def play():
        return "You win!"
    
  return "You lose"
+
+def win_player(player1,computer):
+    # Return True if the player wins  
+    # Stone wins scissors(Stone > Scissors).
+    # Paper wins Stone(Paper > Stone).
+    # Scissors wins paper(Scissors > Paper).
+    
+    if ((player1 == "stone" and computer == "scissors")  
+             or (player1 == "paper" and computer == "stone")
+              or (player1 == "scissors" and computer =="paper")) :
+          return True
+      
+    else :
+        return False
+    
+
+print(play())
+
