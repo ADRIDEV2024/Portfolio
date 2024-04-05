9173bdcdb71f376e06c043c3e79dbba9f1dd008b
AdriDev <adriangomis34@gmail.com>
2024-04-05 12:09:16 +0200

    Adding function

--- FunnyGames/GuessTheNumber/GuessTheNumber.py
+++ FunnyGames/GuessTheNumber/GuessTheNumber.py
@@ -12,10 +12,23 @@ def level_dificulty(level_chosen:str):
     else:
         attempts = 5
         return HARD_LEVEL_ATTEMPTS 
+    
+def check_answer(guess_number,answer:int,attempts:int):
+    if guess_number<answer:
+        print("Wrong!,the number you are looking for is greater")
+        return attempts-1
+    elif guess_number>answer:
+        print("Wrong!,the number you are looking for is lower")
+        return attempts-1
+        
 
 print("I have a number between 1 to 100 in my mind")
 answer = random.randint(1,100)
 
 print(answer)
+
 level = input("Chosse level dificulty...type <<easy>> or <<hard>>: ")
+attempts = level_dificulty(level)
+print(f"You have {attempts}remaining to guess my number.")
+guess_number = input(int("Guess a number: "))
 
