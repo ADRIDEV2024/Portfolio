14ff207b9c74aeb769ae954a65ae9f5f9ab4eb4b
AdriDev <adriangomis34@gmail.com>
2024-04-09 13:07:26 +0200

    Adding things in repo

---
+++ MyFavouriteProjects/PasswordEncrypter/Password_Encrypt.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+
+context = CryptContext(
+    # Alghoritm method to hash the password
+    schemes=["pbkdf2_sha256"], 
+    default="pbkdf2_sha256",
+    pbkdf2_sha256_default_rounds= 70000 # Total of iterations to reduce possible crackings
+)
+
+password = "name_234?-!"
+
+cipher_password = context.encrypt(password)
+print(cipher_password)
+
