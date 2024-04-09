75122d320e57d7d3587f565568455000b65c71a7
AdriDev <adriangomis34@gmail.com>
2024-04-09 12:18:11 +0200

    Editing project

--- FunnyGames/GuessTheNumber/GuessTheNumber.py
+++ FunnyGames/GuessTheNumber/GuessTheNumber.py
@@ -7,13 +7,14 @@ HARD_LEVEL_ATTEMPTS = 5
 def level_dificulty(level_chosen:str):
     
     if level_chosen == "easy":
-        attempts = 10
         return EASY_LEVEL_ATTEMPTS 
-    else:
-        attempts = 5
+    elif level_chosen == "hard":
         return HARD_LEVEL_ATTEMPTS 
+    else:
+        return
     
 def check_answer(guess_number,answer:int,attempts:int):
+    
     if guess_number<answer:
         print("Wrong!,the number you are looking for is greater")
         return attempts-1
@@ -21,19 +22,27 @@ def check_answer(guess_number,answer:int,attempts:int):
         print("Wrong!,the number you are looking for is lower")
         return attempts-1
         
+def play():
+    
+        print("I have a number between 1 to 100 in my mind")
+        answer = random.randint(1,100)
 
-print("I have a number between 1 to 100 in my mind")
-answer = random.randint(1,100)
+        print(answer)
 
-print(answer)
+        level = input("Chosse level dificulty...type <<easy>> or <<hard>>: ")
+        attempts = level_dificulty(level)
+        
+        while guess_number != answer:
 
-level = input("Chosse level dificulty...type <<easy>> or <<hard>>: ")
-attempts = level_dificulty(level)
-while guess_number != answer:
+            print(f"You have {attempts}remaining to guess my number.")
+            guess_number = input(int("Guess a number: "))
+            answer= check_answer(guess_number,answer,attempts)
 
-    print(f"You have {attempts}remaining to guess my number.")
-    guess_number = input(int("Guess a number: "))
-    answer= check_answer(guess_number,answer,attempts)
+        if attempts == 0:
+            print("Sorry,your time to guess has finish. You lose :( ")
+            return
 
-if attempts == 0:
-    print("Sorry,your time to guess has finish. You lose :( ")
+        elif guess_number != answer:
+            print("Guess again")
+
+play()
