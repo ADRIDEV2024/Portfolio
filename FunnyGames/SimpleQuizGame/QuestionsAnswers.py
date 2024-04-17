3048d148c0d405bb91de9258fa0e11f2acec4312
AdriDev <adriangomis34@gmail.com>
2024-04-17 12:50:45 +0200

    Rename things

---
+++ FunnyGames/SimpleQuizGame/QuestionsAnswers.py
@@ -0,0 +1,20 @@
+
+questions = [ 
+{"Question": "Which is not a Python datatype?", "Answer":"B"},
+{"Question": "Using the Set type in Python you can ...", "Answer":"C"},
+{"Question": "What can you do with Jupyter notebook?", "Answer":"C"},
+{"Question": "What is the meaning of PEP8?", "Answer":"A"},
+{"Question": "What is a decorator in Python?", "Answer":"D"}
+]
+
+options = [["A. Float", "B. Char", "C. Bool", "D. List"],
+           ["A. Order a sequence of numbers", "B. Change datatype of a variable",
+            "C. Have a list of elements with any duplicate value or element",
+            "D. You can´t use a set in Python"],
+           ["A. Migrate data to a database", "B. You can make an amazing web page",
+            "C. You can create and share code in more than 40 programming languages",
+            "D. Install Java frameworks and libraries"],
+           ["A. "]
+           
+
+]
