9d83d092d92fd4dce03f5dfc62b499b4fa74b90c
AdriDev <adriangomis34@gmail.com>
2024-04-17 12:47:39 +0200

    Editing my projects

--- FunnyGames/SimpleQuizGame/SimpleQuiz.py
+++ FunnyGames/SimpleQuizGame/SimpleQuiz.py
@@ -4,30 +4,30 @@ print("PYTHON SIMPLE QUIZ GAME")
 print()
 print("?????????????")
 
-questions = [ 
-{"Question": "Which is not a Python datatype?", "Answer":"B"},
-{"Question": "Using the Set type in Python you can ...", "Answer":"C"},
-{"Question": "What can you do with Jupyter notebook?", "Answer":"C"},
-{"Question": "What is the meaning of PEP8?", "Answer":"A"},
-{"Question": "What is a decorator in Python?", "Answer":"D"}
-]
 
-options = [["A. Float", "B. Char", "C."]
-           
-
-]
-
-def check_answer(user_guess,correct_answer):
+def check_answer(user_guess:str,correct_answer:str):
     if user_guess == correct_answer:
         print("Correct!")
     else:
         print("Wrong :( ")
 
 for question in range(len(questions)):
- print("????????????")
+ print("*************************")
  print(questions[question]["Question"])
  for i in options :
      print(i)
-
+     
+score = 0
 guess = input("Enter the answer (A,B,C or D)").upper()
 is_correct = check_answer(guess,[questions]["Answer"])
+if is_correct:
+    print("Correct!")
+    score+=1
+else :
+    print("Upps,maybe is Wrong")
+    print(f"The correct answer is {questions[question]["Answer"]}")
+    score-=1
+print(f"Your current score is {score}/{question+1}")    
+
+print(f"Your have given {score} correct answers")
+print(f"Your score is {(score/len(questions))*100}%")
