dbeb1a3e4cdc117d90f16a5349e86027a07d4a66
Adrian <adriangomis34@gmail.com>
2024-06-09 12:06:18 +0200

    Update urls.py

--- LanguageApp/App/urls.py
+++ LanguageApp/App/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 
-urlpatterns = [
+    urlpatterns = [
     path('', views.index, name="index"),
     path('signup', views.signup, name='signup'),
     path('login/', views.login, name='login'),
@@ -13,5 +13,5 @@ urlpatterns = [
     path("favorite_language/", views.favorite_language, name="select_favorite_language"),
     path("language/<int:language_id>/", views.language, name="language"),
     path("lessons_detail/<int:lesson_id>/", views.lesson_detail, name="lesson_detail"),
-    path("create_community_post/", views.create_community_post, name="create_community_post"),
+    ),
 ]
