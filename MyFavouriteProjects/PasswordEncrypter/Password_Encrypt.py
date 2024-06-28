472f6295491b74121b79191283181e9a1349106c
Adrian <adriangomis34@gmail.com>
2024-06-28 17:41:03 +0200

    Update Password_Encrypt.py

--- MyFavouriteProjects/PasswordEncrypter/Password_Encrypt.py
+++ MyFavouriteProjects/PasswordEncrypter/Password_Encrypt.py
@@ -4,10 +4,10 @@ context = CryptContext(
     # Alghoritm method to hash the password
     schemes = ["pbkdf2_sha256"], 
     default = "pbkdf2_sha256",
-    pbkdf2_sha256_default_rounds = 70000 # Total of iterations to reduce possible crackings
+    pbkdf2_sha256_default_rounds = 30000 # Total of iterations to reduce possible crackings
 )
 
-password = "name_234?-!"
+password = " "
 
 cipher_password = context.hash(password)
 print(cipher_password)
