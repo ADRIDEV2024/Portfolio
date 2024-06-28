168023cc0ff4f7d98bf451366abecf7b73183412
Adrian <adriangomis34@gmail.com>
2024-06-28 17:51:51 +0200

    Update EmailMessage.py

--- AutomateYourEmail/EmailMessage.py
+++ AutomateYourEmail/EmailMessage.py
@@ -9,10 +9,8 @@ email_password = os.environ.get("Email Password")
 email_sender = "adridev2024@gmail.com"
 email_receiver = "receiverperson@gmail.com"
 
-
 subject = " Read this email please! Really important content!"
 body = "I need to say you something about last day, I am Adrian and I think..."
-
 
 email = EmailMessage()
 email["FROM"] = "adridev2024@gmail.com"
@@ -23,7 +21,7 @@ email.set_content(body) # We define and save the email body using the Content Ma
 # Apply SSL security protocol to protect the email and send it to the server in a secure way 
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtpgmail.com", 6000, context=context) as smtp:
+with smtplib.SMTP_SSL("smtpgmail.com", 5000, context=context) as smtp:
     smtp.login(email_sender, email_password)
     smtp.sendmail(email_sender, email_receiver, email.as_string())
 
