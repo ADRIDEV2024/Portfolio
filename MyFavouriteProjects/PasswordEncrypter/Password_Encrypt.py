7c7448deb07d85235b9eea70ae55e6884920ec23
Adrian <adriangomis34@gmail.com>
2024-06-28 17:40:40 +0200

    Update Password_Encrypt.py

--- MyFavouriteProjects/PasswordEncrypter/Password_Encrypt.py
+++ MyFavouriteProjects/PasswordEncrypter/Password_Encrypt.py
@@ -2,8 +2,8 @@ from passlib.context import CryptContext
 
 context = CryptContext(
     # Alghoritm method to hash the password
-    cipher_alghoritm =["pbkdf2_sha256"], 
-    default ="pbkdf2_sha256",
+    schemes = ["pbkdf2_sha256"], 
+    default = "pbkdf2_sha256",
     pbkdf2_sha256_default_rounds = 70000 # Total of iterations to reduce possible crackings
 )
 
