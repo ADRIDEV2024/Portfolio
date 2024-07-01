633c16cf11bc32d19c94737b3e13b5b365261b11
Adrian <adriangomis34@gmail.com>
2024-07-01 11:32:06 +0200

    Update YoutubeDownloader.py

--- OtherProjects/YoutubeDownloader/YoutubeDownloader.py
+++ OtherProjects/YoutubeDownloader/YoutubeDownloader.py
@@ -10,16 +10,16 @@ def Download_Video(url, save_path):
      """""
      
      try:
-        
         yt = YouTube(url)
-        streams = yt.streams.filter(progressive=True, file_extension= "mp4")
+        streams = yt.streams.filter(progressive, file_extension= "mp4")
         highest_res_stream = streams.get_highest_resolution()
         highest_res_stream.download(output_path=save_path)
         print("Video downloaded succesfully! ")
-            
+        
      except Exception as error:
         print(error)
     
+            
 url = ""
 save_path = "C:/User/Downloads" # Example directory path to save video
 
