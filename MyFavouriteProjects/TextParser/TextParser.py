ccfcf5fbcbf01499af6ecdfcdc1223003f43f466
Adrian <adriangomis34@gmail.com>
2024-07-05 13:15:10 +0200

    Update TextParser.py

--- MyFavouriteProjects/TextParser/TextParser.py
+++ MyFavouriteProjects/TextParser/TextParser.py
@@ -5,42 +5,44 @@ Create a program that analizes text and get :
 - Longer word
 - Total number of sentences in that text
 - Average word length
-
 """
 
 import re
+from typing import List, Tuple
 
 
+def count_words(words: List[str]) -> int:
+    return len([word for word in words if word])
 
-def parse(text: str) -> str:
+def count_letters(words: List[str]) -> int:
+    return sum(len(re.sub(r"[^\w]", "", word)) for word in words if word)
 
-    words_count = 0
-    letter_count = 0
-    sentences_count = 0
+def count_sentences(words: List[str]) -> int:
+    return sum(1 for word in words if "." in word)
+
+def find_longest_words(words: List[str]) -> List[str]:
     longest_word = []
-
-    words = text.replace("\n", " ").split(" ")
-
     for word in words:
+        current_word = re.sub(r"[^\w]", "", word)
+        if len(current_word) == 0:
+            continue
 
-        if len(word) != 0:
+        if len(longest_word) == 0 or len(longest_word[0]) == len(current_word):
+            longest_word.append(current_word)
+        elif len(current_word) > len(longest_word[0]):
+            longest_word.clear()
+            longest_word.append(current_word)
+    return longest_word
 
-           if "." in word:
-                sentences_count += 1
+def parse(text: str) -> Tuple[int, int, int, List[str]]:
+    words = text.replace("\n", " ").split(" ")
+    words_count = count_words(words)
+    letter_count = count_letters(words)
+    sentences_count = count_sentences(words)
+    longest_word = find_longest_words(words)
+    
+    return words_count, letter_count, sentences_count, longest_word
 
-            current_word = re.sub(r"[^\w]", "", word)
-
-            print(current_word)
-            
-            words_count += 1
-            letter_count += len(current_word)
-
-            if len(longest_word) == 0 or len(longest_word[0]) == len(current_word):
-                longest_word.append(current_word)
-                
-            elif len(current_word) > len(longest_word[0]):
-                longest_word.clear()
-                longest_word.append(current_word)
 
     print(f"Total number of words is : {words_count}\n")
     print(f"The Average word length is: {letter_count // words_count}\n")
