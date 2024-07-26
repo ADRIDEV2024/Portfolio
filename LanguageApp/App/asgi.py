b018f2cdfa4941f42f23ece2178a2eced471331f
Adrian <adriangomis34@gmail.com>
2024-07-26 11:14:08 +0200

    Update asgi.py

--- LanguageApp/App/asgi.py
+++ LanguageApp/App/asgi.py
@@ -1,6 +1,5 @@
 import os
 from django.core.asgi import get_asgi_application
-import daphne
 
 # This code initializes my web application with ASGI settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Language.settings')
